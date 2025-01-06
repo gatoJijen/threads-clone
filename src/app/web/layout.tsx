9393458f0603c5a7171397d7e14fc8476c39d03f
Santiago <gato.jijen01@gmail.com>
2025-01-06 01:43:09 -0500

    se agrega vercel speed insights

--- src/app/web/layout.tsx
+++ src/app/web/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function WebLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} background-2 antialiased`}
       >
+        <SpeedInsights/>
         {children}
       </body>
     </html>
