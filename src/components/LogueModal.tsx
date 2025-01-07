4d6d6e98b34690f845c389872e2db2d7c523b993
Santiago <gato.jijen01@gmail.com>
2025-01-06 20:42:53 -0500

    f

--- src/components/LogueModal.tsx
+++ src/components/LogueModal.tsx
@@ -8,10 +8,12 @@ interface ModalProps {
     active: boolean
 }
 
-const LogueModal:React.FC<ModalProps> = ({active}) => {
+const LogueModal: React.FC<ModalProps> = ({ active }) => {
     const router = useRouter();
-        const [error, setError] = useState("");
-        const [loading, setLoading] = useState(false); // Estado para manejar la carga
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [error, setError] = useState("");
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [loading, setLoading] = useState(false); // Estado para manejar la carga
     const googleRegister = async () => {
         try {
             const result = await signInWithPopup(auth, provider);
@@ -37,29 +39,29 @@ const LogueModal:React.FC<ModalProps> = ({active}) => {
     return (
         <div className='w-[200vw] absolute h-svh'>
 
-        
-        {active?(
-            <article className='background-1 rounded-xl w-[520px] h-[313px] absolute top-[25%] right-[18%] flex flex-col gap-4 items-center justify-center p-4'>
-                <header className='flex flex-col justify-center items-center gap-2'>
-                    <h1 className=' text-white text-3xl font-bold'>
-                        Di más con Threads
-                    </h1>
-                    <p className='text-center text-white text-opacity-30 w-[360px]'>Únete a Threads para compartir ideas, enterarte de lo que pasa, seguir a tu gente y mucho más.</p>
-                </header>
-                <button
-                    onClick={googleRegister}
-                    className='background-1 mt-6 flex gap-4 items-center justify-between border border-white border-opacity-20 px-4 w-[370px] py-4 rounded-[12px] fs-1'
-                    disabled={loading}
-                >
-                    <picture className='background-image-1 w-[45px] h-[45px] inline-block'></picture>
-                    <h2 className='text-base font-bold'>Continuar con Instagram</h2>
-                    <article className='rotate-180'>
-                        <ContinuarL />
-                    </article>
-                </button>
-            </article>):("")
 
-        }
+            {active ? (
+                <article className='background-1 rounded-xl w-[520px] h-[313px] absolute top-[25%] right-[18%] flex flex-col gap-4 items-center justify-center p-4'>
+                    <header className='flex flex-col justify-center items-center gap-2'>
+                        <h1 className=' text-white text-3xl font-bold'>
+                            Di más con Threads
+                        </h1>
+                        <p className='text-center text-white text-opacity-30 w-[360px]'>Únete a Threads para compartir ideas, enterarte de lo que pasa, seguir a tu gente y mucho más.</p>
+                    </header>
+                    <button
+                        onClick={googleRegister}
+                        className='background-1 mt-6 flex gap-4 items-center justify-between border border-white border-opacity-20 px-4 w-[370px] py-4 rounded-[12px] fs-1'
+                        disabled={loading}
+                    >
+                        <picture className='background-image-1 w-[45px] h-[45px] inline-block'></picture>
+                        <h2 className='text-base font-bold'>Continuar con Instagram</h2>
+                        <article className='rotate-180'>
+                            <ContinuarL />
+                        </article>
+                    </button>
+                </article>) : ("")
+
+            }
         </div>
     )
 }
